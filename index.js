95d462a7675c9a4f21ca1243ff814ce4481b5421
missing6 <115633829+missing6@users.noreply.github.com>
2023-12-07 15:58:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const displayText = document.querySelector("#nText");
-const inputField = document.querySelector("input");
-// Add the event listener
-inputField.addEventListener('input', function(event) {
-	// Update the displayText content with the input field value
-	displayText.textContent = 'Input changed to: ' + event.target.value;
-	
-	// Change the color of the displayText to red
-	displayText.style.color = 'red';
-  });
+
