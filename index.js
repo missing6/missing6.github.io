45fda641192471ec61e49d8f527ab790f02c8420
missing6 <115633829+missing6@users.noreply.github.com>
2023-12-13 13:45:12 +0700

    update

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var displayText = document.getElementById('displayText');
 // Add the event listener
 inputField.addEventListener('input', function(event) {
   // Update the displayText content with the input field value
-  displayText.textContent = 'Input changed to: ' + event.target.value;
+  displayText.textContent = event.target.value;
 });
